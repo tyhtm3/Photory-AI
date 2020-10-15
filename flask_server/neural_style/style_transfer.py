f399db8faf31bcd789303ad2394ba6b1212f7779
방소윤 <bbangso0322@gmail.com>
2020-10-15 17:28:03 +0900

    S03P31A205-42/AI-bbangso : test neural style

--- flask_server/neural_style/style_transfer.py
+++ flask_server/neural_style/style_transfer.py
@@ -2,7 +2,6 @@ import tensorflow as tf
 
 import matplotlib.pyplot as plt
 import matplotlib as mpl
-
 mpl.rcParams['figure.figsize'] = (12,12)
 mpl.rcParams['axes.grid'] = False
 
@@ -10,18 +9,55 @@ import numpy as np
 import PIL.Image
 import time
 import functools
+import tensorflow_hub as hub
 
 def tensor_to_image(tensor):
     tensor = tensor*255
-    tensor = np.array(tensor, dtype=np.unit8)
+    tensor = np.array(tensor, dtype=np.uint8)
     if np.ndim(tensor)>3:
         assert tensor.shape[0] == 1
         tensor = tensor[0]
     return PIL.Image.fromarray(tensor)
+
+
+def load_img(path_to_img):
+    max_dim = 512
+    img = tf.io.read_file(path_to_img)
+    img = tf.image.decode_image(img, channels=3)
+    img = tf.image.convert_image_dtype(img, tf.float32)
+
+    shape = tf.cast(tf.shape(img)[:-1], tf.float32)
+    long_dim = max(shape)
+    scale = max_dim / long_dim
+
+    new_shape = tf.cast(shape * scale, tf.int32)
+
+    img = tf.image.resize(img, new_shape)
+    img = img[tf.newaxis, :]
+    return img
+
+def imshow(image, title=None):
+    if len(image.shape) > 3:
+        image = tf.squeeze(image, axis=0)
+
+    plt.imshow(image)
+    if title:
+        plt.title(title)
+
+hub_module = hub.load('https://tfhub.dev/google/magenta/arbitrary-image-stylization-v1-256/1')
 
 content_path = tf.keras.utils.get_file('YellowLabradorLooking_new.jpg', 'https://storage.googleapis.com/download.tensorflow.org/example_images/YellowLabradorLooking_new.jpg')
 
 # https://commons.wikimedia.org/wiki/File:Vassily_Kandinsky,_1913_-_Composition_7.jpg
 style_path = tf.keras.utils.get_file('kandinsky5.jpg','https://storage.googleapis.com/download.tensorflow.org/example_images/Vassily_Kandinsky%2C_1913_-_Composition_7.jpg')
 
-print("11111",content_path)
+content_path = 'baby_dog.JPG'
+
+content_image = load_img(content_path)
+style_image = load_img(style_path)
+
+stylized_image = hub_module(tf.constant(content_image), tf.constant(style_image))[0]
+img = tensor_to_image(stylized_image)
+
+plt.imshow(img)
+plt.show()
