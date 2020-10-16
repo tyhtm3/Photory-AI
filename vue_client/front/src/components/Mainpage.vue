2c5b975a32339ebb98e670b4b2a6730f8933c2ad
이다현 <ekgus511@naver.com>
2020-10-16 16:01:52 +0900

    S03P31A205-44/Front-Dahyeon : nav 반응형 추가

--- vue_client/front/src/components/Mainpage.vue
+++ vue_client/front/src/components/Mainpage.vue
@@ -1,13 +1,17 @@
 <template>
-  <div>
-    
+  <div id="Main">
+    <h1>This is an Main page</h1>
   </div>
 </template>
 
 <script>
-
+export default {
+  name: 'Mainpage',
+}
 </script>
 
 <style>
-
+#Main {
+ background-color: #f8f8f0;
+}
 </style>
