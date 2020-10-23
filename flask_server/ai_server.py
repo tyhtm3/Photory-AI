0e7dd0b41759d582e07f60309c4a1e26237a8fa7
방소윤 <bbangso0322@gmail.com>
2020-10-23 17:32:04 +0900

    S03P31A205-58/AI-bbangso: Update flask server

--- flask_server/ai_server.py
+++ flask_server/ai_server.py
@@ -7,6 +7,24 @@ app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
 CORS(app)
 
+# 임시
+content_path = 'neural_style/content/female_knight.jpg'
+content_image = utils.load_image(content_path,max_size=None)
+
+style_model = 'neural_stlye/models.wave.ckpt'
+
+# session setting
+soft_config = tf.ConfigProto(allow_soft_placement=True)
+soft_config.gpu_options.allow_growth = True # to deal with large image
+sess = tf.Session(config=soft_config)
+
+transformer = style_transfer_tester.StyleTransferTester(session=sess,
+                                                    model_path=style_model,
+                                                    content_image=content_image,
+                                                    )
+
+output_image = transformer.test()
+
 @app.route('/')
 def index_page():
     return "AI server!"
@@ -14,22 +32,10 @@ def index_page():
 @app.route('/test')
 def test_path():
     # load image and style model
-    content_path = 'neural_style/content/female_knight.jpg'
-    content_image = utils.load_image(content_path,max_size=None)
 
-    style_model = 'neural_stlye/models.wave.ckpt'
-    # session setting
-    soft_config = tf.ConfigProto(allow_soft_placement=True)
-    soft_config.gpu_options.allow_growth = True # to deal with large image
-    sess = tf.Session(config=soft_config)
 
-    transformer = style_transfer_tester.StyleTransferTester(session=sess,
-                                                        model_path=style_model,
-                                                        content_image=content_image,
-                                                        )
-
-    print(transformer.model_path)
     return 'good!'
-    
+
 if __name__=='__main__':
-    app.run(host='0.0.0.0')
+    #app.run(host='0.0.0.0')
+    app.run()
