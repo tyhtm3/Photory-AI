e539a629905535395028fad2c3cf3c3c412b980d
방소윤 <bbangso0322@gmail.com>
2020-10-23 10:25:34 +0900

    S03P31A205-58/AI-bbangso : Test style transfer on Ai server

--- flask_server/ai_server.py
+++ flask_server/ai_server.py
@@ -1,7 +1,7 @@
 from flask import Flask, request
 from flask_cors import CORS
 import tensorflow as tf
-from neural_style import style_transfer_tester
+from neural_style import style_transfer_tester, utils
 
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
@@ -11,5 +11,25 @@ CORS(app)
 def index_page():
     return "AI server!"
 
+@app.route('/test')
+def test_path():
+    # load image and style model
+    content_path = 'neural_style/content/female_knight.jpg'
+    content_image = utils.load_image(content_path,max_size=None)
+
+    style_model = 'neural_stlye/models.wave.ckpt'
+    # session setting
+    soft_config = tf.ConfigProto(allow_soft_placement=True)
+    soft_config.gpu_options.allow_growth = True # to deal with large image
+    sess = tf.Session(config=soft_config)
+
+    transformer = style_transfer_tester.StyleTransferTester(session=sess,
+                                                        model_path=style_model,
+                                                        content_image=content_image,
+                                                        )
+
+    print(transformer.model_path)
+    return 'good!'
+    
 if __name__=='__main__':
     app.run(host='0.0.0.0')
