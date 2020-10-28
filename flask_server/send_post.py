840e7720e94441da3ac9fab32452dd080777d76b
방소윤 <bbangso0322@gmail.com>
2020-10-28 17:17:39 +0900

    S03P31A205-62/AI-bbangso : Complete style transfer test

--- flask_server/send_post.py
+++ flask_server/send_post.py
@@ -5,7 +5,7 @@ import time
 def send_post():
     start = time.time()
     params = {
-        "url": "http://ecotopia.hani.co.kr/files/attach/images/69/406/492/d2.jpg",
+        "url": "https://japan-magazine.jnto.go.jp/jnto2wm/wp-content/uploads/1701_japanese_dog_main.jpg",
     }
     res = requests.post("http://127.0.0.1:5000/style", data=json.dumps(params))
     print("time to return : " + str(time.time() - start))
