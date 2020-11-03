432215646822a9b57fb93ca03073979e510d2578
이다현 <ekgus511@naver.com>
2020-11-03 13:20:47 +0900

    S03P31A205-45/Front-Dahyeon Loading.io적용중

--- vue_client/front/src/components/Mainpage.vue
+++ vue_client/front/src/components/Mainpage.vue
@@ -3,7 +3,12 @@
     <!-- Mainpage 화면클때 -->
     <div id="hill"  class="d-none d-sm-block">
       <img src="@/assets/hill.png" style="max-width: -webkit-fill-available;">
+      <!-- <div
+        style="background:url(../assets/loading/wave.png)">
+        ????
+      </div> -->
     </div>
+    
     <!-- Mainpage 화면 작을때 -->
     <div id="hill" class="d-sm-none">
       <img src="@/assets/hill_mini.png" style="max-width: -webkit-fill-available;">
