8ad53fa0ab65f14e7dea72131157979249ecdb87
DahyeonL <ekgus511@naver.com>
2020-11-04 17:03:01 +0900

    S03P31A205-47/Front-Dahyeon share개별 페이지

---
+++ vue_client/front/src/views/SharestoryPage.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <SharestoryPage/>
+  </div>
+</template>
+<script>
+import SharestoryPage from '@/components/SharestoryPage.vue'
+export default {
+  components:{
+   SharestoryPage,
+  }
+}
+</script>
+<style>
+  
+</style>
