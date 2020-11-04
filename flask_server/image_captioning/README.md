b59a30434acde14f3457398654f6867a9cfdcc79
최현우 <jilp1598@daum.net>
2020-11-04 09:31:21 +0900

    S03P31A205-54 / AI-Hyeonwoo : 이미지 캡셔닝 코드 업로드

--- flask_server/image_captioning/README.md
+++ flask_server/image_captioning/README.md
@@ -1,8 +1,11 @@
-대략 90% 완성되었습니다. 완성되면 주피터 파일을 파이썬 파일로 변경하고 구축된 모델을 구글 드라이브로 공유하겠습니다.
+100% 완성되었습니다. 구축된 모델은 구글 드라이브로 공유하며 빠른 시일내로 설명서 정리하겠습니다.
 
+구글 드라이브 링크 : https://drive.google.com/drive/folders/1Vp5WR5XFmgc1Fnpc7N6HU4MUhvCNaXfO?usp=sharing
 
 ### 파일 구조
+
 ```sh
 ms_coco_eval.ipynb : 이미지에 대한 이미지 캡셔닝 수행
-mc_coco_modeling.ipynb : 이미지 캡셔닝 모델링 프로그램 
+mc_coco_modeling.ipynb : 이미지 캡셔닝 모델링 프로그램
 train_captions.pickle : 학습된 캡션을 담고 있는 파일
+```
---
+++ flask_server/image_captioning/ms_coco_eval.py
@@ -0,0 +1,225 @@
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+from sklearn.model_selection import train_test_split
+from sklearn.utils import shuffle
+
+import collections
+import random
+import re
+import numpy as np
+import os
+import time
+import json
+from glob import glob
+from PIL import Image
+import pickle
+#tf.config.run_functions_eagerly(True)
+#tf.enable_eager_execution()
+#tf.executing_eagerly() 
+# pip install matplotlib sklearn 
+class BahdanauAttention(tf.keras.Model):
+  def __init__(self, units):
+    super(BahdanauAttention, self).__init__()
+    self.W1 = tf.keras.layers.Dense(units)
+    self.W2 = tf.keras.layers.Dense(units)
+    self.V = tf.keras.layers.Dense(1)
+
+  def call(self, features, hidden):
+    # features(CNN_encoder output) shape == (batch_size, 64, embedding_dim)
+
+    # hidden shape == (batch_size, hidden_size)
+    # hidden_with_time_axis shape == (batch_size, 1, hidden_size)
+    hidden_with_time_axis = tf.expand_dims(hidden, 1)
+
+    # attention_hidden_layer shape == (batch_size, 64, units)
+    attention_hidden_layer = (tf.nn.tanh(self.W1(features) +
+                                         self.W2(hidden_with_time_axis)))
+
+    # score shape == (batch_size, 64, 1)
+    # This gives you an unnormalized score for each image feature.
+    score = self.V(attention_hidden_layer)
+
+    # attention_weights shape == (batch_size, 64, 1)
+    attention_weights = tf.nn.softmax(score, axis=1)
+
+    # context_vector shape after sum == (batch_size, hidden_size)
+    context_vector = attention_weights * features
+    context_vector = tf.reduce_sum(context_vector, axis=1)
+
+    return context_vector, attention_weights
+
+
+class CNN_Encoder(tf.keras.Model):
+    # Since you have already extracted the features and dumped it using pickle
+    # This encoder passes those features through a Fully connected layer
+    def __init__(self, embedding_dim):
+        super(CNN_Encoder, self).__init__()
+        # shape after fc == (batch_size, 64, embedding_dim)
+        self.fc = tf.keras.layers.Dense(embedding_dim)
+
+    def call(self, x):
+        x = self.fc(x)
+        x = tf.nn.relu(x)
+        return x
+    
+    
+class RNN_Decoder(tf.keras.Model):
+  def __init__(self, embedding_dim, units, vocab_size):
+    super(RNN_Decoder, self).__init__()
+    self.units = units
+
+    self.embedding = tf.keras.layers.Embedding(vocab_size, embedding_dim)
+    self.gru = tf.keras.layers.GRU(self.units,
+                                   return_sequences=True,
+                                   return_state=True,
+                                   recurrent_initializer='glorot_uniform')
+    self.fc1 = tf.keras.layers.Dense(self.units)
+    self.fc2 = tf.keras.layers.Dense(vocab_size)
+
+    self.attention = BahdanauAttention(self.units)
+
+  def call(self, x, features, hidden):
+    # defining attention as a separate model
+    context_vector, attention_weights = self.attention(features, hidden)
+
+    # x shape after passing through embedding == (batch_size, 1, embedding_dim)
+    x = self.embedding(x)
+
+    # x shape after concatenation == (batch_size, 1, embedding_dim + hidden_size)
+    x = tf.concat([tf.expand_dims(context_vector, 1), x], axis=-1)
+    
+    # passing the concatenated vector to the GRU
+    output, state = self.gru(x)
+
+    # shape == ()
+    x = self.fc1(output)
+
+    # x shape == (batch_size * max_length, hidden_size)
+    x = tf.reshape(x, (-1, x.shape[2]))
+
+    # output shape == (batch_size * max_length, vocab)
+    x = self.fc2(x)
+
+    return x, state, attention_weights
+
+  def reset_state(self, batch_size):
+    return tf.zeros((batch_size, self.units))
+
+
+
+def evaluate(image):
+    attention_plot = np.zeros((max_length, attention_features_shape))
+
+    hidden = decoder.reset_state(batch_size=1)
+    temp_input = tf.expand_dims(load_image(image)[0], 0)
+    img_tensor_val = image_features_extract_model(temp_input)
+    img_tensor_val = tf.reshape(img_tensor_val, (img_tensor_val.shape[0], -1, img_tensor_val.shape[3]))
+    
+    features = encoder(img_tensor_val)
+    dec_input = tf.expand_dims([tokenizer.word_index['<start>']], 0)
+       
+    result = []
+
+    for i in range(max_length):
+        predictions, hidden, attention_weights = decoder(dec_input, features, hidden)
+
+        attention_plot[i] = tf.reshape(attention_weights, (-1, )).numpy()
+        predicted_id = tf.random.categorical(predictions, 1)[0][0].numpy()
+        result.append(tokenizer.index_word[predicted_id])
+
+        if tokenizer.index_word[predicted_id] == '<end>':
+            return result, attention_plot
+
+        dec_input = tf.expand_dims([predicted_id], 0)
+
+    attention_plot = attention_plot[:len(result), :]
+    return result, attention_plot
+
+
+def plot_attention(image, result, attention_plot):
+    temp_image = np.array(Image.open(image))
+
+    fig = plt.figure(figsize=(10, 10))
+
+    len_result = len(result)
+    for l in range(len_result):
+        temp_att = np.resize(attention_plot[l], (8, 8))
+        ax = fig.add_subplot(len_result//2, len_result//2, l+1)
+        ax.set_title(result[l])
+        img = ax.imshow(temp_image)
+        ax.imshow(temp_att, cmap='gray', alpha=0.6, extent=img.get_extent())
+
+    plt.tight_layout()
+    plt.show()
+    
+def load_image(image_path):
+    img = tf.io.read_file(image_path)
+    img = tf.image.decode_jpeg(img, channels=3)
+    img = tf.image.resize(img, (299, 299))
+    img = tf.keras.applications.inception_v3.preprocess_input(img)
+    return img, image_path
+
+
+
+
+with open('checkpoints/train3/train_captions.pickle','rb') as fr:
+    train_captions = pickle.load(fr)
+    
+max_length = 24000 # 학습한 캡션의 갯수, 학습 이미지의 갯수랑 동일
+
+top_k = 8000
+embedding_dim = 256
+units = 512
+vocab_size = top_k + 1
+features_shape = 2048
+attention_features_shape = 64
+
+encoder = CNN_Encoder(embedding_dim)
+decoder = RNN_Decoder(embedding_dim, units, vocab_size)
+
+optimizer = tf.keras.optimizers.Adam()
+
+
+checkpoint_path = "./checkpoints/train3"
+ckpt = tf.train.Checkpoint(encoder=encoder,
+                           decoder=decoder,
+                           optimizer = optimizer)
+ckpt_manager = tf.train.CheckpointManager(ckpt, checkpoint_path, max_to_keep=5)
+
+
+start_epoch = 0
+if ckpt_manager.latest_checkpoint:
+  start_epoch = int(ckpt_manager.latest_checkpoint.split('-')[-1])*5
+  # restoring the latest checkpoint in checkpoint_path
+  ckpt.restore(ckpt_manager.latest_checkpoint)
+  print(start_epoch,"epoch start")
+    
+    
+loss_plot = []
+
+
+image_model = tf.keras.applications.InceptionV3(include_top=False,
+                                                weights='imagenet')
+new_input = image_model.input
+hidden_layer = image_model.layers[-1].output
+
+image_features_extract_model = tf.keras.Model(new_input, hidden_layer)
+
+tokenizer = tf.keras.preprocessing.text.Tokenizer(num_words=top_k,
+                                                  oov_token="<unk>",
+                                                  filters='!"#$%&()*+.,-/:;=?@[\]^_`{|}~ ')
+tokenizer.fit_on_texts(train_captions)
+
+
+tokenizer.word_index['<pad>'] = 0
+tokenizer.index_word[0] = '<pad>'
+
+
+image_url = 'https://tensorflow.org/images/surf.jpg'
+image_extension = image_url[-4:]
+image_path = tf.keras.utils.get_file('image'+image_extension,
+                                     origin=image_url)
+
+result, attention_plot = evaluate(image_path)
+print ('Prediction Caption:', ' '.join(result))
