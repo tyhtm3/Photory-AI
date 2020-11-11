da5cc79e85c530130e4f38b082d263fc71f0959b
방소윤 <bbangso0322@gmail.com>
2020-11-11 11:57:24 +0900

    hotfix

--- flask_server/ai_server.py
+++ flask_server/ai_server.py
@@ -14,8 +14,8 @@ app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
 CORS(app)
 
-
-
+IMAGE_SERVER_URL = "http://127.0.0.1:8000"
+STYLE_PATH = ['wave.ckpt', 'udnie.ckpt', 'ths_scream.ckpt', 'shipwreck.ckpt', 'rain_princess.ckpt', 'la_muse.ckpt']
 
 @app.route('/')
 def index_page():
@@ -24,9 +24,27 @@ def index_page():
 @app.route('/tale', methods=['POST'])
 def tale():
     data = json.loads(request.get_data(), encoding='utf-8')
-    
-    print(data)
+    story_pk = data['story_pk']
+    paths = data['imagePaths']
 
+    cnt = 0
+    for path in paths:
+        # image load from url
+        image_url = IMAGE_SERVER_URL + '/media/' +path
+        res = requests.get(image_url)
+        img = Image.open(BytesIO(res.content))
+        img = np.asarray(img)
+
+        for i in range(6):
+            with tf.Graph().as_default():
+                transformer = style_transfer_tester.StyleTransferTester(
+                    img, 'neural_style/fast_neural_style/' + STYLE_PATH[i]
+                )
+                output = transformer.test()
+            
+            result_path = 'story'+str(story_pk) + '/' + str(cnt) + '_i.jpg'
+            utils.save_image(output, 'static/' + result_path)
+        cnt = cnt + 1
     return "hi"
 
 
