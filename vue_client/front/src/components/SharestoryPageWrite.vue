463162a407f2fad48203c7c6838914894d745227
DahyeonL <ekgus511@naver.com>
2020-11-11 17:35:28 +0900

    S03P31A205-47/Front-Dahyeon write 페이지 작성중

---
+++ vue_client/front/src/components/SharestoryPageWrite.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>lakfjsdljksfldkjsalfjsdl</h1>
+    <EditorTipTap :description="description" :menubar="menubar" :readOnly="readOnly"/>
+  </div>
+</template>
+<script>
+import EditorTipTap from '@/components/EditorTipTap.vue'
+export default {
+  components:{
+    EditorTipTap,
+  },
+  data: () => ({
+    description:"글을 적어주세요",
+    menubar:true,
+    readOnly:false,
+  }),
+}
+</script>
+<style>
+  
+</style>
