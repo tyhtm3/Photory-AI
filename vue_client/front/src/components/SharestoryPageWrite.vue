ad860f590a64c393e906fd027fef09d78d8d5f28
DahyeonL <ekgus511@naver.com>
2020-11-11 17:56:35 +0900

    S03P31A205-47/Front-Dahyeon editpage 구성중

--- vue_client/front/src/components/SharestoryPageWrite.vue
+++ vue_client/front/src/components/SharestoryPageWrite.vue
@@ -1,8 +1,29 @@
 <template>
-  <div>
-    <h1>lakfjsdljksfldkjsalfjsdl</h1>
-    <EditorTipTap :description="description" :menubar="menubar" :readOnly="readOnly"/>
-  </div>
+  <v-container>
+    <v-row style="height:10vh">
+      <v-col>
+        <div class="back">
+          <img src="@/assets/loading/wave.png" style="max-width: -webkit-fill-available; position: absolute; bottom: 0;left: 0;">
+        </div>
+      </v-col>
+    </v-row>
+    <v-row justify="center">
+      <v-col cols="8" id="title">
+        <v-text-field
+            v-model="title"
+            :rules="rules"
+            counter="25"
+            label="Outlined"
+            outlined
+            rounded
+          ></v-text-field>
+      </v-col>
+      <v-col cols="8" id="editor">
+      <EditorTipTap :description="description" :menubar="menubar" :readOnly="readOnly"/>
+      </v-col>
+    </v-row>
+
+  </v-container>
 </template>
 <script>
 import EditorTipTap from '@/components/EditorTipTap.vue'
@@ -18,5 +39,12 @@ export default {
 }
 </script>
 <style>
-  
+#title{
+
+}
+ #editor{
+  background-color: white;
+  border: 10px solid rgb(220, 247, 158);
+  border-radius: 80px / 40px;
+ }
 </style>
