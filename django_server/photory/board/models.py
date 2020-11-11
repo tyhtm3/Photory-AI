08dd108b7d897344dd962920db4eecbcef280bb0
tyhtm3 <tyhtm3@gmail.com>
2020-11-12 02:58:03 +0900

    S03P31A205-72/featureback/board : temp

--- django_server/photory/board/models.py
+++ django_server/photory/board/models.py
@@ -1,17 +1,22 @@
 from django.db import models
 from django.conf import settings
-
+from storys.models import Story
 # Create your models here.
 class Article(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    nickname = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    bookcover = models.ImageField()
+    story = models.ForeignKey(Story,on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     writer = models.CharField(max_length=100)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    category = models.IntegerField()
 
 class Comment(models.Model):
+    nickname = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    story = models.ForeignKey(Story,on_delete=models.CASCADE)
     content = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
