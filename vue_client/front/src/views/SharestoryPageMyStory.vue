072d71790e6ea29141e7416e6102a05101e0cea4
이다현 <ekgus511@naver.com>
2020-11-16 12:56:15 +0900

    story보기

---
+++ vue_client/front/src/views/SharestoryPageMyStory.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <SharestoryPageMyStory/>
+  </div>
+</template>
+<script>
+// import SharestoryPage from '@/components/SharestoryPageDialog.vue'
+// import SharestoryPage from '@/components/SharestoryPage.vue'
+import SharestoryPageMyStory from '@/components/SharestoryPageMyStory.vue'
+export default {
+  components:{
+   SharestoryPageMyStory,
+  }
+}
+</script>
+<style>
+  
+</style>
